60ecade3a1b2eea74c98da1ab4b76b19948bcf48
youqi <youqi@pinduoduo.com>
2021-03-04 10:58:13 +0800

    3.3

--- 前端题目/实现类/柯里化/curry-acc-v1.js
+++ 前端题目/实现类/柯里化/curry-acc-v1.js
@@ -1,11 +1,24 @@
-curried(1)(2)(3); // => [1, 2, 3]
-curried(1, 2)(3); // => [1, 2, 3]
-curried(1, 2, 3); // => [1, 2, 3]
+// curried(1)(2)(3); // => [1, 2, 3]
+// curried(1, 2)(3); // => [1, 2, 3]
+// curried(1, 2, 3); // => [1, 2, 3]
 
+// var addAll = (a, b, c) => {
+//     return a + b + c
+// }
+
+// var curry = func => (...args) => {
+//     if(func.length < args.length){
+//         return () => {
+            
+//         }
+//     }
+//     return 
+// }
 var curry = func => (...args) => {
-    if (func.length < args.length) {
-        return () => {
-            return func(...args.concat(Array.from(arguments)))
+    if (func.length > args.length) {
+        return (...params) => {
+            var _args = [...args,...params]
+            return curry(func)(..._args)
         }
     }
     return func(...args)
@@ -17,3 +30,4 @@ var addAll = (a, b, c) => {
 
 var curryFn = curry(addAll)
 console.log(curryFn(1, 2)(3))
+console.log(curryFn(1,2,3,4))
