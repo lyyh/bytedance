a678e2976aecd0f54fcc78926bc601dca90c39c5
youqi <youqi@pinduoduo.com>
2021-03-09 09:56:52 +0800

    3.9

---
+++ 前端题目/概念类/事件循环&宏任务&微任务/index_5.js
@@ -0,0 +1,15 @@
+const pro = new Promise((resolve, reject) => {
+    const innerpro = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(1);
+        }, 0);
+        console.log(2);
+        resolve(3);
+    });
+    innerpro.then(res => console.log(res));
+    resolve(4);
+    console.log("pro");
+})
+pro.then(res => console.log(res));
+console.log("end");
+// 2 => pro => end => 3 => 4
